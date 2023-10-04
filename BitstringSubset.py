5354523da00d9306ff2f4b45c94546a5cd7f814f
farnsolo <97287314+farnsolo@users.noreply.github.com>
2023-10-04 09:43:38 +0100

    Add files via upload

---
+++ BitstringSubset.py
@@ -0,0 +1,47 @@
+# Empty list to be filled
+test = []
+
+# Ask the user to input set
+n = int(input("Enter Number of elements : "))
+print("Enter elements - one by one")
+
+ # iterate till range
+for i in range(0, n):
+    temp = int(input())
+    # adding the element
+    test.append(temp)
+    
+    
+# Bool that keeps track whether or not the while loop is active
+continueLoop = True
+# Empty subset list
+subset = []
+
+
+# While loop that allows the user to repeat the sequence
+while(continueLoop == True):
+     # User enter bitstring
+     bitString = input("Enter Whole Bitstring: ")
+     
+     # iterates through the length of test list - compares contents of Bitstring cell to "1"
+     for i in range(len(test)):
+        if(bitString[i] == "1"):
+            # If 1 exists in that index position, then append content of the index position in test list to subset list
+            subset.append(test[i])
+        
+        # Prints out members of subset
+        for item in subset:
+            print(item)
+        
+        # Asks user is they want to repeat sequence
+        enterNew = input("Continue? Y/N ")
+        if(enterNew == "N" ):
+            quit()
+        else:
+            continueLoop = True
+
+    
+        
+    
+                
+                
