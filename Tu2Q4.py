b688c9e90a63e45b88a3f73f4f92793cce6690ad
Farhan Khan <97287314+farnsolo@users.noreply.github.com>
2023-10-13 18:58:07 +0100

    Add files via upload

---
+++ Tu2Q4.py
@@ -0,0 +1,31 @@
+# The algorithm specified in question does not require the elements of the superset to be known
+# For the purpose of this program, the cardinality of the set will be asked 
+cardinalityA = int(input("Enter number of elements in Set A: "))
+
+# The question requires the bitstrings of two subsets of baseSet
+
+# NOTE TO SELF - Len function only works on string - so convert other data types to str to use len function
+subsetbx = input("Enter subsetbx: ")
+
+while len(str(subsetbx)) != cardinalityA:
+    subsetbx = input("Enter the correct number of members - The subset must equal the cardinality of the baseSet: ")
+
+# Repeat same for subset Y
+subsetby = input("Enter subsetby: ")
+
+while len(str(subsetby)) != cardinalityA:
+    subsetby = input("Enter the correct number of members - The subset must equal the cardinality of the baseSet: ")
+    
+print("Given the finite set A - subsetbx\subsetby for  bx belongs to A and by belongs to A")
+
+# bxDy (bx DIFFERENCE by) is the set which have the members of x\y
+bxDy = ""
+i = 0
+for i in range(0,cardinalityA):
+    if(subsetbx[i] == '1' and subsetby[i] == '0'):
+        bxDy = bxDy + '1'
+    else:
+        bxDy = bxDy + '0'
+    
+print("Bx\y: " + bxDy)
+
